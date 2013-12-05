aa1406b16b84d895b449832ac5295a9c78eee087
Atle Prange <atle.prange@gmail.com>
2013-12-05 23:41:58 +0100

    Better handling of validations

--- src/main/java/kembe/StreamEvent.java
+++ src/main/java/kembe/StreamEvent.java
@@ -2,6 +2,9 @@ package kembe;
 
 import fj.F;
 import fj.P1;
+import fj.Show;
+import fj.data.NonEmptyList;
+import fj.data.Validation;
 
 public abstract class StreamEvent<A> {
 
@@ -9,30 +12,62 @@ public abstract class StreamEvent<A> {
         return new F<StreamEvent<A>, StreamEvent<B>>() {
             @Override
             public StreamEvent<B> f(StreamEvent<A> aStreamEvent) {
-                return aStreamEvent.map(f);
+                return aStreamEvent.map( f );
             }
         };
     }
 
-    public static <A> Next<A> next(A value){
-        return new Next<>(value);
+    public static <A, E, B> F<StreamEvent<A>, StreamEvent<B>> liftV(final F<A, Validation<NonEmptyList<E>, B>> f, final Show<E> msgConverter) {
+        return new F<StreamEvent<A>, StreamEvent<B>>() {
+            @Override public StreamEvent<B> f(StreamEvent<A> aStreamEvent) {
+                return aStreamEvent
+                        .fold(
+                                new F<A, StreamEvent<B>>() {
+                                    @Override public StreamEvent<B> f(A a) {
+                                        Validation<NonEmptyList<E>, B> v = f.f( a );
+                                        if (v.isSuccess())
+                                            return StreamEvent.next( v.success() );
+                                        else
+                                            return StreamEvent.error( new Exception( Show.nonEmptyListShow( msgConverter ).showS( v.fail() ) ) );
+                                    }
+                                }, new F<Exception, StreamEvent<B>>() {
+                                    @Override public StreamEvent<B> f(Exception e) {
+                                        return StreamEvent.error( e );
+                                    }
+                                }, new P1<StreamEvent<B>>() {
+                                    @Override public StreamEvent<B> _1() {
+                                        return StreamEvent.done();
+                                    }
+                                }
+                        );
+            }
+        };
     }
 
-    public static <A> F<A,Next<A>> next(){
+    public static <A> Next<A> next(A value) {
+        return new Next<>( value );
+    }
+
+    public static <A> F<A, Next<A>> next() {
         return new F<A, Next<A>>() {
             @Override public Next<A> f(A a) {
-                return next(a);
+                return next( a );
             }
         };
     }
 
-    public static <A> Error<A> error(Exception e){
-        return new Error<>(e);
+    public static <A> Error<A> error(Exception e) {
+        return new Error<>( e );
     }
 
-    public static <A> Done<A> done(){
+    public static <A> Done<A> done() {
         return new Done<>();
     }
+
+    public <E, B> StreamEvent<B> mapV(F<A, Validation<NonEmptyList<E>, B>> f,Show<E> msgConverter) {
+        return liftV( f,msgConverter ).f( this );
+    }
+
     public abstract void effect(EventStreamHandler handler);
 
     public abstract <T> T fold(F<A, T> onNext, F<Exception, T> onError, P1<T> onDone);
@@ -72,12 +107,12 @@ public abstract class StreamEvent<A> {
 
         @Override
         public <T> T fold(F<A, T> onNext, F<Exception, T> onError, P1<T> onDone) {
-            return onError.f(e);
+            return onError.f( e );
         }
 
         @Override
         public <B> StreamEvent<B> map(F<A, B> f) {
-            return new Error<>(e);
+            return new Error<>( e );
         }
     }
 
@@ -97,12 +132,12 @@ public abstract class StreamEvent<A> {
 
         @Override
         public <T> T fold(F<A, T> onNext, F<Exception, T> onError, P1<T> onDone) {
-            return onNext.f(value);
+            return onNext.f( value );
         }
 
         @Override
         public <B> StreamEvent<B> map(F<A, B> f) {
-            return new Next<>(f.f(value));
+            return new Next<>( f.f( value ) );
         }
     }
 
