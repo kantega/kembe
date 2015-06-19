c10f2184e760839fc51d1bc0fdb914393089642b
Atle Prange <atle.prange@kantega.no>
2015-06-19 12:17:48 +0200

    Moved to java 8

--- src/test/java/kembe/sim/stat/RandWaitTest.java
+++ src/test/java/kembe/sim/stat/RandWaitTest.java
@@ -17,45 +17,33 @@ import java.util.Random;
 
 public class RandWaitTest {
 
-    static final Arbitrary<LocalTime> arbLocalTime = Arbitrary.arbitrary( Gen.choose( 0, 23 ).bind( Gen.choose( 0, 59 ), new F<Integer, F<Integer, LocalTime>>() {
-        public F<Integer, LocalTime> f(final Integer hour) {
-            return new F<Integer, LocalTime>() {
-                public LocalTime f(final Integer minute) {
-                    return new LocalTime( hour, minute, 0, 0 );
-                }
-            };
-        }
-    } ) );
+    static final Arbitrary<LocalTime> arbLocalTime =
+            Arbitrary.arbitrary( Gen.choose( 0, 23 ).bind( Gen.choose( 0, 59 ), hour -> minute -> new LocalTime( hour, minute, 0, 0 ) ) );
 
-    static final DateTime now = new LocalTime( 6, 0 ).toDateTime( Time.now() );
+    static final DateTime now =
+            new LocalTime( 6, 0 ).toDateTime( Time.now() );
 
-    static final RandWait waitUntilSevenTilNine = RandWait.waitUntilBetween( new LocalTime( 7, 0 ), new LocalTime( 9, 0 ) );
+    static final RandWait waitUntilSevenTilNine =
+            RandWait.waitUntilBetween( new LocalTime( 7, 0 ), new LocalTime( 9, 0 ) );
 
-    static final DateTime earliest = new LocalTime( 7, 0 ).toDateTime( now );
+    static final DateTime earliest =
+            new LocalTime( 7, 0 ).toDateTime( now );
 
-    static final DateTime latest = new LocalTime( 9, 0 ).toDateTime( now );
+    static final DateTime latest =
+            new LocalTime( 9, 0 ).toDateTime( now );
 
-    static final Rand<DateTime> randNext = waitUntilSevenTilNine.after( now );
+    static final Rand<DateTime> randNext =
+            waitUntilSevenTilNine.after( now );
 
-    static final Random random = new Random( 5 );
+    static final Random random =
+            new Random( 5 );
 
-    static final Arbitrary<DateTime> nextBetweenSevenAdnNine = Arbitrary.arbitrary( Gen.gen( new F<Integer, F<fj.test.Rand, DateTime>>() {
-        public F<fj.test.Rand, DateTime> f(final Integer integer) {
-            return new F<fj.test.Rand, DateTime>() {
-                public DateTime f(final fj.test.Rand rand) {
-                    return randNext.next( random );
-                }
-            };
-        }
-    } ) );
+    static final Arbitrary<DateTime> nextBetweenSevenAdnNine =
+            Arbitrary.arbitrary( Gen.gen( integer -> rand -> randNext.next( random ) ) );
 
 
-    Property p2 = Property.property( nextBetweenSevenAdnNine, new F<DateTime, Property>() {
-        public Property f(final DateTime instant) {
-
-            return Property.prop( !instant.isBefore( earliest ) && !instant.isAfter( latest ) );
-        }
-    } );
+    Property p2 =
+            Property.property( nextBetweenSevenAdnNine, instant -> Property.prop( !instant.isBefore( earliest ) && !instant.isAfter( latest ) ) );
 
     //@Test
     public void checkProperties() {
