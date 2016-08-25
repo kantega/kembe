1cf91c4f8cbc2c4d204e0092a5155003de2cc3f2
Espen Fossen <espenaf@junta.no>
2016-08-25 15:00:20 +0200

    Fixed stack overflow issue by using stack safe actor.

--- src/main/java/kembe/sim/runner/Scheduler.java
+++ src/main/java/kembe/sim/runner/Scheduler.java
@@ -5,6 +5,7 @@ import fj.control.parallel.Actor;
 import fj.control.parallel.Strategy;
 import fj.function.Effect1;
 import kembe.sim.Timed;
+import kembe.util.Actors;
 import kembe.util.Order;
 import org.joda.time.DateTime;
 
@@ -49,31 +50,31 @@ public abstract class Scheduler {
     }
 
     private static <T> Actor<T> orderedActor(final Strategy<Unit> s, final Ord<T> ord, final Effect1<T> ea) {
-        return Actor.actor( Strategy.<Unit>seqStrategy(), new Effect1<T>() {
+        return Actors.stackSafeQueueActor(Strategy.<Unit>seqStrategy(), new Effect1<T>() {
 
             // Lock to ensure the actor only acts on one message at a time
-            AtomicBoolean suspended = new AtomicBoolean( true );
+            AtomicBoolean suspended = new AtomicBoolean(true);
 
-            AtomicLong atomicLong = new AtomicLong( 0 );
+            AtomicLong atomicLong = new AtomicLong(0);
 
-            ConcurrentSkipListSet<Numbered<T>> mbox = new ConcurrentSkipListSet<>( Order.toComparator( numberedOrd( ord ) ) );
+            ConcurrentSkipListSet<Numbered<T>> mbox = new ConcurrentSkipListSet<>(Order.toComparator(numberedOrd(ord)));
 
             // Product so the actor can use its strategy (to act on messages in other threads,
             // to handle exceptions, etc.)
             P1<Unit> processor = new P1<Unit>() {
-                @Override public Unit _1() {
+                @Override
+                public Unit _1() {
                     try {
                         // get next item from queue
                         Numbered<T> a = mbox.pollFirst();
                         // if there is one, process it
                         if (a != null) {
-                            ea.f( a.value );
+                            ea.f(a.value);
                             // try again, in case there are more messages
-                            s.par( this );
-                        }
-                        else {
+                            s.par(this);
+                        } else {
                             // clear the lock
-                            suspended.set( true );
+                            suspended.set(true);
                             // work again, in case someone else queued up a message while we were holding the lock
                             work();
                         }
@@ -85,18 +86,19 @@ public abstract class Scheduler {
             };
 
             // Effect's body -- queues up a message and tries to unsuspend the actor
-            @Override public void f(T a) {
-                mbox.add( new Numbered<>( atomicLong.incrementAndGet(), a ) );
+            @Override
+            public void f(T a) {
+                mbox.add(new Numbered<>(atomicLong.incrementAndGet(), a));
                 work();
             }
 
             // If there are pending messages, use the strategy to run the processor
             protected void work() {
-                if (!mbox.isEmpty() && suspended.compareAndSet( true, false )) {
-                    s.par( processor );
+                if (!mbox.isEmpty() && suspended.compareAndSet(true, false)) {
+                    s.par(processor);
                 }
             }
-        } );
+        });
     }
 
     private static <T> Ord<Numbered<T>> numberedOrd(Ord<T> ordT) {
